e185c83584980f66d7df5686e043c7014e598e24
TheLocust3 <jake.kinsella@gmail.com>
2023-08-09 07:07:07 -0400

    First pass a buckets with policies

---
+++ blob/lib/database/buckets.ml
@@ -0,0 +1,69 @@
+open! Base
+open! Core
+
+open Common.Api
+open Model
+
+let make ~name ~policy =
+  let out_policy = policy |> Yojson.Safe.from_string |> Bucket.Policy.of_yojson |> Result.ok |> Option.value ~default: Bucket.Policy.deny_all in
+  Bucket.({ name = name; policy = out_policy })
+
+let migrate_query = [%rapper
+  execute {sql|
+    CREATE TABLE buckets (
+      name TEXT NOT NULL UNIQUE PRIMARY KEY,
+      policy TEXT NOT NULL
+    )
+  |sql}
+]
+
+let rollback_query = [%rapper
+  execute {sql|
+    DROP TABLE buckets
+  |sql}
+]
+
+let create_query = [%rapper
+  execute {sql|
+    INSERT INTO buckets (name, policy)
+    VALUES (%string{name}, %string{policy})
+  |sql}
+]
+
+let delete_query = [%rapper
+  execute {sql|
+    DELETE FROM buckets
+    WHERE name = %string{name}
+  |sql}
+]
+
+let by_name_query = [%rapper
+  get_opt {sql|
+    SELECT @string{buckets.name}, @string{buckets.policy}
+    FROM buckets
+    WHERE name = %string{name}
+  |sql}
+  function_out
+](make)
+
+let migrate connection =
+  let query = migrate_query () in
+    query connection |> Error.Database.or_print
+
+let rollback connection =
+  let query = rollback_query () in
+    query connection |> Error.Database.or_print
+
+let by_name name connection =
+  let query = by_name_query ~name: name in
+    query connection |> Error.Database.or_error_opt
+
+let create bucket connection =
+  let Bucket.({ name; policy }) = bucket in
+  let out_policy = policy |> Bucket.Policy.to_yojson |> Yojson.Safe.to_string in
+  let query = create_query ~name: name ~policy: out_policy in
+    query connection |> Error.Database.or_error
+
+let delete name connection =
+  let query = delete_query ~name: name in
+    query connection |> Error.Database.or_error
---
+++ blob/lib/model/bucket.ml
@@ -0,0 +1,39 @@
+open! Base
+open! Core
+
+module Policy = struct
+  module Effect = struct
+    type t = Allow | Deny [@@deriving yojson]
+  end
+
+  module Action = struct
+    type t = All | Read | Write [@@deriving yojson]
+  end
+
+  module Principal = struct
+    type t = All | UserId of string [@@deriving yojson]
+  end
+
+  module Statement = struct
+    type t = {
+      effect : Effect.t;
+      action : Action.t;
+      principal : Principal.t;
+    } [@@deriving yojson]
+  end
+
+  type t = {
+    head : Statement.t;
+    rest : Statement.t list;
+  } [@@deriving yojson]
+
+  let deny_all = {
+    head = Statement.({ effect = Effect.Deny; action = Action.All; principal = Principal.All });
+    rest = [];
+  }
+end
+
+type t = {
+  name : string;
+  policy : Policy.t;
+} [@@deriving yojson]
