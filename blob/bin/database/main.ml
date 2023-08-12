88f22198fc6d3b4fe09a867aa44db8dd73cd69a6
TheLocust3 <jake.kinsella@gmail.com>
2023-08-12 18:00:54 -0400

    Create default bucket

--- blob/bin/database/main.ml
+++ blob/bin/database/main.ml
@@ -2,13 +2,37 @@ open! Base
 open! Core
 open Lwt
 
+open Model.Bucket
+
 let migrate () =
-  let%lwt _ = Database.Connect.connect () in
+  let%lwt connection = Database.Connect.connect () in
+
+  let email = "jake.kinsella@gmail.com" in
+  let user_id = email |> Uuidm.v5 Uuidm.ns_url |> Uuidm.to_string in
+  let policy = Policy.({
+    head = Policy.Statement.({
+      effect = Policy.Effect.Allow;
+      action = Policy.Action.All;
+      principal = Policy.Principal.UserId user_id;
+    });
+    rest = []
+  }) in
+  let bucket = { name = email; policy = policy } in
+
+  let%lwt _ = Database.Buckets.migrate connection in
+  let%lwt _ = Database.Blobs.migrate connection in
+
+  let%lwt _ = Database.Buckets.create bucket connection in
+
   let _ = Stdlib.Printf.printf("Migration complete\n") in
   return ()
 
 let rollback () =
-  let%lwt _ = Database.Connect.connect () in
+  let%lwt connection = Database.Connect.connect () in
+  
+  let%lwt _ = Database.Blobs.rollback connection in
+  let%lwt _ = Database.Buckets.rollback connection in
+
   let _ = Stdlib.Printf.printf("Rollback complete\n") in
   return ()
 
