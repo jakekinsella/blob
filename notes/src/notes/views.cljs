81487aa422cac126cc500dd32cf286223602fe85
TheLocust3 <jake.kinsella@gmail.com>
2023-09-04 19:24:24 -0400

    Get basic login redirect working

--- notes/src/notes/views.cljs
+++ notes/src/notes/views.cljs
@@ -3,7 +3,7 @@
    [re-frame.core :as re-frame]
    [notes.styles :as styles]
    [notes.subs :as subs]
-   ))
+   [central :as central]))
 
 (defn main []
   [:div
@@ -12,8 +12,10 @@
     "Hello world"]
    ])
 
+(def to_login (str central/Constants.central.root "/login?redirect=" (js/encodeURIComponent central/Constants.notes.root)))
+
 (defn login []
-  [:div "login"])
+  [:> central/Redirect {:to to_login}])
 
 (def routes
   [["/"
