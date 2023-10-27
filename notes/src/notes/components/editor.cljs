9c32a20a00fae73a7f384164a21e36531a8865e3
TheLocust3 <jake.kinsella@gmail.com>
2023-10-27 06:37:32 -0400

    Another tweak

--- notes/src/notes/components/editor.cljs
+++ notes/src/notes/components/editor.cljs
@@ -103,8 +103,8 @@
                           (reset! height (+ canvas-height screen-height))
                           (save)))))
                 mount (fn []
-                        (do (set! (-> ref .-current .-width) (math/ceil (* 2 (:width @body))))
-                            (set! (-> ref .-current .-height) (math/ceil (* 2 (:height @body))))
+                        (do (set! (-> ref .-current .-width) (math/ceil (* 1.6 (:width @body))))
+                            (set! (-> ref .-current .-height) (math/ceil (* 1.6 (:height @body))))
                             (set! (-> ref .-current .-style .-width) (str (:width @body) "px"))
                             (set! (-> ref .-current .-style .-height) (str (:height @body) "px"))
                             (dorun (map (fn [line] (draw-line (:drawing line) (:points line))) (:lines @body)))))]
