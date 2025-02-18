28bb11b21f573bfa2aa1b34adc07edbd359f5646
jakekinsella <jake.kinsella@gmail.com>
2025-02-18 11:52:06 -0500

    Get image build working again

--- build/deploy.sh
+++ build/deploy.sh
@@ -6,11 +6,12 @@ echo "Deploying to ${CONTROL_PLANE_IP} @ $VERSION"
 
 ./build/push.sh $VERSION
 
+PASSWORD=$(ssh ubuntu@"${CONTROL_PLANE_IP}" "aws ecr get-login-password --region us-east-1")
 NODES=$(ssh ubuntu@"${CONTROL_PLANE_IP}" "sudo kubectl get nodes -owide" | grep -v NAME | awk '{print $6}')
 for node in $NODES
 do
   echo $node
-  ssh -A ubuntu@"${CONTROL_PLANE_IP}" ssh -o "StrictHostKeyChecking=no" ubuntu@"${node}" "sudo ./ecr_refresh.sh"
+  ssh -A ubuntu@"${CONTROL_PLANE_IP}" ssh -o "StrictHostKeyChecking=no" ubuntu@"${node}" "sudo ./ecr_refresh.sh $PASSWORD"
 done
 
 ssh ubuntu@"${CONTROL_PLANE_IP}" "sudo kubectl apply -f ~/cluster/blob/"
